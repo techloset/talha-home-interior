9afafd1318d720fc8039dd0ab57fafb6043e67c5
talhaunas1 <mtalha4600@gmail.com>
2023-08-11 00:07:56 +0500

    working on responsiveness 2 sections left

--- src/pages/happyCustomer/happyCustomer.jsx
+++ src/pages/happyCustomer/happyCustomer.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const happyCustomer = () => {
     return (
-        <div className='realtive  mt-[10%]  md:grid md:mt-[7%] font-popins'>
+        <div className=' mt-[10%]  md:grid md:mt-[7%] font-popins'>
 
-            <div class=' mx-5  md:text-center bg-blue-500'>
-                <p className=' text-[18px] bg-yellow-500 md:text-[36px] font-[500] md:m-8'>Our Happy Customers</p>
-                <p className=' text-[12px] mt-[10px] bg-red-500 md:w-[60%] md:m-auto md:text-[15px] lg:text-[18px] md:font-[400]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet
+            <div class=' mx-5  md:text-center '>
+                <p className=' text-[18px]  md:text-[36px] font-[500] md:m-8'>Our Happy Customers</p>
+                <p className=' text-[12px] mt-[10px]  md:w-[60%] md:m-auto md:text-[15px] lg:text-[18px] md:font-[400]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet
                     luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim</p>
             </div>
 
-            <div className='absolute z-0 md:grid md:grid-cols-12 md:text-center md:mt-[3%]' >
+            <div className='realtive  z-0 md:grid md:grid-cols-12 md:text-center md:mt-[3%]' >
 
                 <div className='bg-blue-500 md:col-span-2'>
 
@@ -21,7 +21,7 @@ const happyCustomer = () => {
 
 
                 </div>
-                <div className='md:hidden  bg-green-500'>
+                <div className='md:hidden  '>
                     <img className='h-full w-[90%] mx-auto' src="/images/happyCustomerMobile.png" alt="" />
 
 
@@ -33,9 +33,9 @@ const happyCustomer = () => {
 
 
 
-            <div className='bg-gray-500 realtive z-10 md:bg-white md:w-[50%]  md:mt-[-100px] md:m-auto md:text-center'>
+            <div className=' absolute w-[75%] left-14 sm:static  z-10 mt-[-100px]   bg-white md:w-[50%]  md:mt-[-100px] md:m-auto md:text-center'>
 
-                <img className=' mt-[-20px] bg-red-500  h-[40px] md:h-[78px] md:mt-[-35px] m-auto ' src="/images/happyCustomer4.png" alt="" />
+                <img className=' mt-[-20px]  h-[40px] md:h-[78px] md:mt-[-35px] m-auto ' src="/images/happyCustomer4.png" alt="" />
 
                 <p className='hidden sm:block   md:text-[14px] lg:text-[20px] lg:font-[400] md:w-[80%] md:m-auto '>Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     Culpa ipsam iusto maxime molestias? Sint nam illo molestiae deserunt
@@ -43,7 +43,7 @@ const happyCustomer = () => {
                     recusandae numquam tenetur, corrupti asperiores aut! Ipsum, libero.</p>
 
 
-                <p className=' md:hidden bg-blue-500 w-[75%] mx-auto  text-[10px]  text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, 
+                <p className=' md:hidden p-5  w-[80%] mx-auto  text-[10px]  text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, 
                 lectus magna fringilla urna, porttitor</p>
 
                 <p className='text-[14px] text-center font-[600] md:text-[20px] lg:text-[22px]'>-Jeo Stanlee</p>
